9d128347bb42d9a7b033916659c9444734a8af72
kumachan-mis <y.s11nov@fuji.waseda.jp>
2020-04-09 13:25:21 +0900

    add sample/ to .prettierignore

--- sample/i18n.ts
+++ sample/i18n.ts
@@ -1,34 +1,5 @@
-const en = {
-  okButtonName: 'Done',
-  welcome: 'Hi, ${userName}',
-  pages: { user: "${userName}'s page", help: 'Help', contact: 'Contact' },
-};
-const ja = {
-  okButtonName: '完了',
-  welcome: 'こんにちは、${userName}さん',
-  pages: { user: '${userName}のページ', help: 'ヘルプ', contact: 'お問い合わせ' },
-};
+const en = { okButtonName: "Done", welcome: "Hi, ${userName}", pages: { user: "${userName}'s page", help: "Help", contact: "Contact",  },  };
+const ja = { okButtonName: "完了", welcome: "こんにちは、${userName}さん", pages: { user: "${userName}のページ", help: "ヘルプ", contact: "お問い合わせ",  },  };
 let currentLang = ja;
-export const l10n = {
-  okButtonName: currentLang.okButtonName,
-  welcome: function (userName: string): string {
-    return currentLang.welcome.replace('${userName}', userName);
-  },
-  pages: {
-    user: function (userName: string): string {
-      return currentLang.pages.user.replace('${userName}', userName);
-    },
-    help: currentLang.pages.help,
-    contact: currentLang.pages.contact,
-  },
-};
-export function changeCurrentLang(lang: 'en' | 'ja'): void {
-  switch (lang) {
-    case 'en':
-      currentLang = en;
-      break;
-    case 'ja':
-      currentLang = ja;
-      break;
-  }
-}
+export const l10n = { okButtonName: currentLang.okButtonName, welcome: function (userName: string): string { return currentLang.welcome.replace("${userName}", userName); }, pages: { user: function (userName: string): string { return currentLang.pages.user.replace("${userName}", userName); }, help: currentLang.pages.help, contact: currentLang.pages.contact,  },  };
+export function changeCurrentLang(lang: "en" | "ja"): void { switch (lang) { case "en": currentLang = en; break; case "ja": currentLang = ja; break;  } }
