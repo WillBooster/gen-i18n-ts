d9f82027ae7dc157a1ab267918fb9ac7baa3f755
kumachan-mis <y.s11nov@fuji.waseda.jp>
2020-04-09 18:18:12 +0900

    refine test.sh

--- __tests__/test.sh
+++ __tests__/test.sh
@@ -6,8 +6,8 @@ rm -Rf ${tempdir}
 mkdir ${tempdir}
 
 for i18n in test-fixtures/i18n-*; do
-    out=$(basename ${i18n}).ts
-    yarn start --i ${i18n} --o ${tempdir}/${out} --default en
+    out=${tempdir}/$(basename ${i18n}).ts
+    yarn start --i ${i18n} --o ${out} --default en
 done
 
 yarn jest
