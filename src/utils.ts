2e8b62a51be1ac3166c43ec71b5886813b3e3304
kumachan-mis <y.s11nov@fuji.waseda.jp>
2020-08-16 15:58:42 +0900

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ export function isString(obj: unknown): obj is string {
   return typeof obj === 'string';
 }
 
-export function isObject(obj: unknown): obj is Record<string, any> {
+export function isObject(obj: unknown): obj is Record<string, unknown> {
   return obj && typeof obj === 'object' && !Array.isArray(obj);
 }
 
