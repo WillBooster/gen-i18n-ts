90e3c7cd2e7d55132ccebde77797b58b32a8cb05
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2021-07-15 12:47:20 +0900

    feat: add -w/--watch flag (#74)

--- src/index.ts
+++ src/index.ts
@@ -10,34 +10,8 @@ import * as utils from './utils';
 import { hideBin } from 'yargs/helpers';
 import yargs from 'yargs/yargs';
 
-export function geni18ts(indir: string, outfile: string, defaultLang: string): void {
-  const langFilePaths = fs
-    .readdirSync(indir)
-    .filter((fileName) => fileName.endsWith('.json'))
-    .map((langFileName) => path.join(indir, langFileName));
-
-  const defaultLangFilePath = path.join(indir, utils.langToFilename(defaultLang));
-
-  if (!langFilePaths.includes(defaultLangFilePath)) throw new Error(ErrorMessages.noDefaultLangFile());
-
-  const typeObj = LangFileConverter.toTypeObj(defaultLangFilePath);
-  const defaultJsonObj = LangFileConverter.toJsonObj(defaultLangFilePath);
-
-  const jsonObjMap: { [lang: string]: unknown } = {};
-  for (const langFilePath of langFilePaths) {
-    console.info(InfoMessages.analyzingLangFile(langFilePath));
-    const lang = utils.filepathToLang(langFilePath);
-    const jsonObj = LangFileConverter.toJsonObj(langFilePath);
-    ObjectAnalyzer.analyze(typeObj, lang, jsonObj, defaultJsonObj);
-    jsonObjMap[lang] = jsonObj;
-  }
-
-  const code = CodeGenerator.gen(typeObj, jsonObjMap, defaultLang);
-  fs.writeFileSync(outfile, code, { encoding: 'utf-8' });
-}
-
 export async function cli(argv: string[]): Promise<void> {
-  const { inputDir, outfile, defaultLang } = await yargs(hideBin(argv)).options({
+  const { inputDir, outfile, defaultLang, watch } = await yargs(hideBin(argv)).options({
     inputDir: {
       type: 'string',
       alias: 'i',
@@ -56,7 +30,46 @@ export async function cli(argv: string[]): Promise<void> {
       describe: 'A name of a default language',
       demandOption: true,
     },
+    watch: {
+      type: 'boolean',
+      alias: 'w',
+      describe: 'Enable watch mode',
+    },
   }).argv;
 
-  geni18ts(inputDir, outfile, defaultLang);
+  if (watch) {
+    fs.watch(inputDir, (event, fileName) => {
+      if (!fileName.endsWith('.json')) return;
+      console.info(`Detect changes in ${inputDir} (${event} on ${fileName})`);
+      genI18ts(inputDir, outfile, defaultLang);
+    });
+  } else {
+    genI18ts(inputDir, outfile, defaultLang);
+  }
+}
+
+export function genI18ts(inputDir: string, outfile: string, defaultLang: string): void {
+  const langFilePaths = fs
+    .readdirSync(inputDir)
+    .filter((fileName) => fileName.endsWith('.json'))
+    .map((langFileName) => path.join(inputDir, langFileName));
+
+  const defaultLangFilePath = path.join(inputDir, utils.langToFilename(defaultLang));
+
+  if (!langFilePaths.includes(defaultLangFilePath)) throw new Error(ErrorMessages.noDefaultLangFile());
+
+  const typeObj = LangFileConverter.toTypeObj(defaultLangFilePath);
+  const defaultJsonObj = LangFileConverter.toJsonObj(defaultLangFilePath);
+
+  const jsonObjMap: { [lang: string]: unknown } = {};
+  for (const langFilePath of langFilePaths) {
+    console.info(InfoMessages.analyzingLangFile(langFilePath));
+    const lang = utils.filepathToLang(langFilePath);
+    const jsonObj = LangFileConverter.toJsonObj(langFilePath);
+    ObjectAnalyzer.analyze(typeObj, lang, jsonObj, defaultJsonObj);
+    jsonObjMap[lang] = jsonObj;
+  }
+
+  const code = CodeGenerator.gen(typeObj, jsonObjMap, defaultLang);
+  fs.writeFileSync(outfile, code, { encoding: 'utf-8' });
 }
