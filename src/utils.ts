34c3aafad5ce3befed35c43c1e329aa46542f510
Kumachan <y.s11nov@fuji.waseda.jp>
2021-10-06 14:56:37 +0900

    feat: support numeric string as key of object (#105)

--- src/utils.ts
+++ src/utils.ts
@@ -6,6 +6,6 @@ export function isObject(obj: unknown): obj is Record<string, unknown> {
   return !!obj && typeof obj === 'object' && !Array.isArray(obj);
 }
 
-export function memberVarName(objectVarName: string, key: string): string {
-  return objectVarName !== '' ? `${objectVarName}.${key}` : key;
+export function getMemberVarName(objectVarName: string, key: string): string {
+  return objectVarName !== '' ? `${objectVarName}[${JSON.stringify(key)}]` : key;
 }
