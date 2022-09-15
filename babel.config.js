8745a7d6970b49164df5c144a33185a924bfd86e
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-09-15 23:15:09 +0900

    .

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = function (api) {
   api.cache(true);
 
-  return require('@willbooster/babel-configs/babel.config.js');
+  return require('@willbooster/babel-configs/babel.config.cjs');
 };
